6660971e9c0bb6117fe9262036da85c24013e95e
Sebastian Sujarchuk <sebastian.sujarchuk@mercadolibre.com>
2019-06-05 15:11:02 -0300

    add examples

---
+++ example/plus_one_square.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sebisujar/piper"
+)
+
+func main() {
+	square := piper.NewSyncStage(func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			n := _n.(int)
+			out <- n * n
+		}
+	})
+
+	plus := piper.NewSyncStage(func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			n := _n.(int)
+			out <- n + 1
+		}
+		time.Sleep(time.Second)
+	})
+
+	output := piper.NewSyncStage(func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			fmt.Println(_n)
+		}
+	})
+
+	p := piper.NewPipeline().
+		AddStage(plus).
+		AddStage(square).
+		AddStage(output)
+
+	p.Run()
+	in := p.In()
+	in <- 1
+	in <- 2
+	in <- 3
+	p.Stop()
+
+	p.Wait()
+	fmt.Println("done")
+}
---
+++ example/splitter.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sebisujar/piper"
+)
+
+func main() {
+	plus := piper.NewBufferedStage(10, func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			n := _n.(int)
+			out <- n + 1
+		}
+	})
+
+	square := piper.NewSyncStage(func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			n := _n.(int)
+			out <- n * n
+		}
+		time.Sleep(2 * time.Second)
+	})
+
+	id := piper.NewSyncStage(func(in chan interface{}, out chan interface{}) {
+		for _n := range in {
+			out <- _n
+		}
+	})
+
+	p1 := piper.NewPipeline().AddStage(id)
+	p2 := piper.NewPipeline().AddStage(square)
+
+	p := piper.NewPipeline().
+		AddStage(plus).
+		Split(*p1, *p2)
+
+	go func() {
+		for x := range p1.Out() {
+			fmt.Println("id: ", x)
+		}
+	}()
+
+	go func() {
+		for x := range p2.Out() {
+			fmt.Println("square: ", x)
+		}
+	}()
+
+	p.Run()
+	in := p.In()
+	in <- 1
+	in <- 2
+	in <- 3
+
+	p.Stop()
+	fmt.Println("will wait")
+	p.Wait()
+	fmt.Println("done ")
+}
